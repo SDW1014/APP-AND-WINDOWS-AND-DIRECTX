28752d07a20f019c4980ebb122cd3a14b452e8cc
SDW1014 <apif730@gmail.com>
2024-07-10 22:23:59 +0900

    [feat] DC에 대한 설명 추가

--- WindowAPP_1/main.cpp
+++ WindowAPP_1/main.cpp
@@ -213,6 +213,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    
     switch (message)
     {
     case WM_COMMAND:
@@ -236,6 +237,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
+
+            // 위치 등은 hwnd를 통해서 사용이 가능하다.
+
+            // DC란 화명네 출력에 필요한 모든 정보를 가지는 데이터 구조체이다. 
+            // GDI모듈에 의해서 관리된다. 
+            // 어떤 폰트를 사용할건가? , 어떤 선의 굵기를 정해줄건가 어떤 색상으로 그려줄건가 등...
+            // 화면 출력에 필요한 모든 경우는 WINAPI에서는 DC를 통해서 작업을 진행할 수 있다. 
+
+
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
             EndPaint(hWnd, &ps);
         }
