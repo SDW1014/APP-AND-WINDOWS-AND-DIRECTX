9c655bd3f1fbbc7434dc0e8ce149f917d893facd
SDW1014 <apif730@gmail.com>
2024-07-12 08:45:00 +0900

    [feat] vector를 사용해서 key의 모든 부분을 상태를 저장해주는 부분을 만들었다. 그리고 정적 변수를 사용하는 부분을 추가하여, 간단한 설명까지 추가하였다.

--- WindowApp/Engine_SOURCE/myInput.h
+++ WindowApp/Engine_SOURCE/myInput.h
@@ -3,7 +3,7 @@
 
 namespace MyApp
 {
-	enum eKeyState
+	enum class eKeyState
 	{
 		Down,
 		Pressed,
@@ -11,12 +11,33 @@ namespace MyApp
 		None,
 	};
 
+	enum class eKeyCode
+	{
+		Q, W, E, R, T, Y, U, I, O, P,
+		A, S, D, F, G, H, J, K, L,
+		Z, X, C, V, B, N, M,
+		End,
+	};
+
 	// key의 상태는 Stay Down Up 3가지 유형으로 나뉘어진다. 
 	class Input
 	{
 	public:
+		struct Key
+		{
+			eKeyCode keyCode;	// 눌려진 키가 무엇인지
+			eKeyState state;	// 눌려진 키의 상태가 무엇인지
+			bool bPressed;		// 눌려진 키가 눌려있는지
+		};
+
 	private:
 		//eKeyState mState; // 0 : Down, 1 : Pressed, 2 : Up, 3 : None
+		static std::vector<Key> mKeys;	// 키들을 저장하는 배열
+
+		// 간단 정적 변수에 대해서 설명
+		// 외부 특히 main함수 밖에서 사용하는건 모든 객체가 공유하고 사용이 가능한 정적 변수이다. 
+		// 근데 Class내부에서 정의한 static 변수는 외부에서 사용이 불가능하다. 
+		// 한마디로 클래스 내부에서만 사용이 가능한 정적 변수이다.
 	};
 }
 
