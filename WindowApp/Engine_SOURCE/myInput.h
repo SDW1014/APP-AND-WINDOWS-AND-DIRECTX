d98d578976ed58f7d373c3a7243266b38ce7be05
SDW1014 <apif730@gmail.com>
2024-07-12 08:33:16 +0900

    [feat,fix] WindowAPP_2안에 myInput을 형성한 문제를 발견, Engine_SOURCE안에 INPUT을 다시 형성하였다.

---
+++ WindowApp/Engine_SOURCE/myInput.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "CommonInclude.h"
+namespace MyApp
+{
+	enum eKeyState
+	{
+		Down,
+		Pressed,
+		Up,
+		None,
+	};
+
+	// key의 상태는 Stay Down Up 3가지 유형으로 나뉘어진다. 
+	class Input
+	{
+	public:
+	private:
+		//eKeyState mState; // 0 : Down, 1 : Pressed, 2 : Up, 3 : None
+	};
+}
+
--- WindowApp/WindowAPP_2/myInput.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace MyApp
-{
-	// key의 상태는 Stay Down Up 3가지 유형으로 나뉘어진다. 
-	class Input
-	{
-	};
-}
-
