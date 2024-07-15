1650a0a85b7012486fe2e23d4535548846919828
SDW1014 <apif730@gmail.com>
2024-07-15 14:43:40 +0900

    [feat] GDI+에 대한 설명을 적었다.

--- WindowApp/Engine_SOURCE/CommonInclude.h
+++ WindowApp/Engine_SOURCE/CommonInclude.h
@@ -23,3 +23,8 @@
 #include <gdiplus.h>  // GDI+를 사용하여 2D 그래픽을 그리기 위한 헤더 파일
 #pragma comment(lib, "gdiplus.lib")  // GDI+ 라이브러리를 링크하기 위한 pragma 지시문
 #pragma comment(lib, "Msimg32.lib")  // 이미지 처리를 위한 Windows 라이브러리
+
+// GDI+는 Windows에서 2D 그래픽을 그리기 위한 API입니다.
+// GDI+를 사용하면 선, 도형, 텍스트, 이미지 등을 쉽게 그릴 수 있습니다.
+// 또한 그라데이션, 투명도, 안티앨리어싱 등의 효과도 적용할 수 있습니다.
+// Windows 프로그래밍에서 그래픽 처리를 위해 널리 사용되는 라이브러리입니다.
