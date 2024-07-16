30c58e9c5049ba0650cf80bbfab64a8b908e42b0
SDW1014 <apif730@gmail.com>
2024-07-16 11:34:00 +0900

    [feat] Change myPlayScene header and CPp

--- WindowApp/Engine_Window/myPlayScene.cpp
+++ WindowApp/Engine_Window/myPlayScene.cpp
@@ -3,44 +3,71 @@
 #include "myPlayer.h"
 #include "myTransform.h"
 #include "mySpriteRenderer.h"
+#include "myInput.h"
+#include "myTitleScene.h"
+#include "mySceneManager.h"
 
 namespace MyApp
 {
+	// 생성자와 소멸자
 	PlayScene::PlayScene()
 	{
 	}
 	PlayScene::~PlayScene()
 	{
 	}
+
+	// 초기화 함수
 	void PlayScene::Initialize()
 	{
-		{
-			Player* bg = new Player();
-			Transform* tr = bg->AddComponent<Transform>();
-			tr->SetPos(Vector2(0, 0));
+		// 플레이어 게임 오브젝트 생성 및 초기화
+		bg = new Player();
+		Transform* tr = bg->AddComponent<Transform>();
+		tr->SetPos(Vector2(0, 0));
+		tr->SetName(L"TR");
 
-			tr->SetName(L"TR");
+		SpriteRenderer* sr = bg->AddComponent<SpriteRenderer>();
+		sr->SetName(L"SR");
+		sr->ImageLoad(L"C:\\Users\\82102\\source\\repos\\2024-07-09\\WindowApp\\Recources\\CloudOcean.png");
 
-			SpriteRenderer* sr = bg->AddComponent<SpriteRenderer>();
-			sr->SetName(L"SR");
-			
-			sr->ImageLoad(L"C:\\Users\\82102\\source\\repos\\2024-07-09\\WindowApp\\Recources\\CloudOcean.png");
-
-
-			AddGameObject(bg);
-		}
+		AddGameObject(bg, eLayerType::BackGround);
 	}
 
+	// 업데이트 함수
 	void PlayScene::Update()
 	{
 		Scene::Update();
 	}
+
+	// 늦은 업데이트 함수
 	void PlayScene::LateUpdate()
 	{
 		Scene::LateUpdate();
+
+		// 'N' 키를 누르면 제목 씬으로 로드
+		if (Input::GetKeyDown(eKeyCode::N))
+		{
+			SceneManager::LoadScene(L"TitleScene");
+		}
 	}
+
+	// 렌더링 함수
 	void PlayScene::Render(HDC hdc)
 	{
 		Scene::Render(hdc);
+		wchar_t str[50] = L"Play Scene";
+		TextOut(hdc, 0, 0, str, 10);
+	}
+
+	// 씬 진입 함수
+	void PlayScene::OnEnter()
+	{
+	}
+
+	// 씬 퇴장 함수
+	void PlayScene::OnExit()
+	{
+		Transform* tr = bg->GetComponent<Transform>();
+		tr->SetPos(Vector2(0, 0));
 	}
 }
--- WindowApp/Engine_Window/myPlayScene.h
+++ WindowApp/Engine_Window/myPlayScene.h
@@ -2,19 +2,26 @@
 #include "..\\Engine_SOURCE\\myScene.h"
 namespace MyApp
 {
+	// PlayScene 클래스는 Scene 클래스를 상속합니다.
 	class PlayScene : public Scene
 	{
 	public:
+		// 생성자와 소멸자를 정의합니다.
 		PlayScene();
 		~PlayScene();
 
+		// Scene 클래스의 가상 함수를 재정의합니다.
 		void Initialize() override;
 		void Update() override;
 		void LateUpdate() override;
 		void Render(HDC hdc) override;
 
+		// OnEnter와 OnExit 함수를 재정의합니다.
+		void OnEnter() override;
+		void OnExit()  override;
+
 	private:
+		// Player 클래스의 포인터를 멤버 변수로 선언합니다.
+		class Player* bg;
 	};
 }
-
-
