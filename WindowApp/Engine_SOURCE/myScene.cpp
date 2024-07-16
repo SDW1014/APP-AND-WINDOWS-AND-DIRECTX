abcaa2cf277f4e65805d2196ede706ecc13a1d2d
SDW1014 <apif730@gmail.com>
2024-07-16 11:20:58 +0900

    [feat] add New Functions in myScene Header and Cpp

--- WindowApp/Engine_SOURCE/myScene.cpp
+++ WindowApp/Engine_SOURCE/myScene.cpp
@@ -2,39 +2,93 @@
 
 namespace MyApp
 {
+	// Scene 생성자
 	Scene::Scene()
-		: mGameObjects{}
+		: mLayers{}
 	{
+		// 모든 레이어를 생성합니다.
+		CreateLayers();
 	}
+	// Scene 소멸자
 	Scene::~Scene()
 	{
 	}
+	// Scene 초기화 함수
 	void Scene::Initialize()
 	{
+		// 모든 레이어를 초기화합니다.
+		for (Layer* layer : mLayers)
+		{
+			if (layer == nullptr)
+				continue;
+
+			layer->Initialize();
+		}
 	}
+	// Scene 업데이트 함수
 	void Scene::Update()
 	{
-		for(GameObject* gameObj : mGameObjects)
+		// 모든 레이어를 업데이트합니다.
+		for (Layer* layer : mLayers)
 		{
-			gameObj->Update();
+			if (layer == nullptr)
+				continue;
+
+			layer->Update();
 		}
 	}
+	// Scene 늦은 업데이트 함수
 	void Scene::LateUpdate()
 	{
-		for(GameObject* gameObj : mGameObjects)
+		// 모든 레이어의 늦은 업데이트를 호출합니다.
+		for (Layer* layer : mLayers)
 		{
-			gameObj->LateUpdate();
+			if (layer == nullptr)
+				continue;
+
+			layer->LateUpdate();
 		}
 	}
+	// Scene 렌더링 함수
 	void Scene::Render(HDC hdc)
 	{
-		for(GameObject* gameObj : mGameObjects)
+		// 모든 레이어를 렌더링합니다.
+		for (Layer* layer : mLayers)
 		{
-			gameObj->Render(hdc);
+			if (layer == nullptr)
+				continue;
+
+			layer->Render(hdc);
 		}
 	}
-	void Scene::AddGameObject(GameObject* gameObject)
+
+	// 특정 레이어에 게임 오브젝트를 추가하는 함수
+	void Scene::AddGameObject(GameObject* gameObj, const eLayerType type)
 	{
-		mGameObjects.push_back(gameObject);
+		mLayers[(UINT)type]->AddGameObject(gameObj);
+	}
+
+	// 모든 레이어를 생성하는 함수
+	void Scene::CreateLayers()
+	{
+		// 모든 레이어 타입에 맞게 벡터의 크기를 조정합니다.
+		mLayers.resize((UINT)eLayerType::Max);
+		// 각 레이어를 생성합니다.
+		for (size_t i = 0; i < (UINT)eLayerType::Max; i++)
+		{
+			mLayers[i] = new Layer();
+		}
+	}
+
+	// 씬이 활성화될 때 호출되는 함수
+	void Scene::OnEnter()
+	{
+
+	}
+
+	// 씬이 비활성화될 때 호출되는 함수
+	void Scene::OnExit()
+	{
+
 	}
 }
--- WindowApp/Engine_SOURCE/myScene.h
+++ WindowApp/Engine_SOURCE/myScene.h
@@ -1,24 +1,43 @@
 #pragma once
-#include "CommonInclude.h"
-#include "myEntity.h"
-#include "myGameObject.h"
+#include "CommonInclude.h" // 공통 포함 파일
+#include "myEntity.h" // 엔티티 클래스
+#include "myGameObject.h" // 게임 오브젝트 클래스
+#include "myLayer.h" // 레이어 클래스
 
 namespace MyApp
 {
+	// Scene 클래스 정의
 	class Scene : public Entity
 	{
 	public:
+		// 생성자
 		Scene();
+		// 소멸자
 		~Scene();
 
+		// 초기화 함수
 		virtual void Initialize();
+		// 업데이트 함수
 		virtual void Update();
+		// 늦은 업데이트 함수
 		virtual void LateUpdate();
+		// 렌더링 함수
 		virtual void Render(HDC hdc);
 
-		void AddGameObject(GameObject* gameObject);
+		// 씬 진입 시 호출
+		virtual void OnEnter();
+		// 씬 퇴장 시 호출
+		virtual void OnExit();
+
+		// 게임 오브젝트를 특정 레이어에 추가
+		void AddGameObject(GameObject* gameObj, const eLayerType type);
+
 	private:
-		std::vector<GameObject*> mGameObjects;
+		// 레이어 생성 함수
+		void CreateLayers();
+
+	private:
+		// 레이어 벡터
+		std::vector<Layer*> mLayers;
 	};
 }
-
