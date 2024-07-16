4d5ad342fc4a2496e6084b255d77b992fc21880d
SDW1014 <apif730@gmail.com>
2024-07-16 10:57:28 +0900

    [feat] Add myTitleScene CPP and Header it is another Scene

---
+++ WindowApp/Engine_Window/myTitleScene.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "..\\Engine_SOURCE\\myScene.h"
+
+namespace MyApp
+{
+    // TitleScene 클래스: Scene을 상속받는 타이틀 씬 클래스
+    class TitleScene : public Scene
+    {
+    public:
+        // 생성자
+        TitleScene();
+        // 소멸자
+        ~TitleScene();
+
+        // 초기화 함수
+        void Initialize() override;
+        // 업데이트 함수
+        void Update() override;
+        // 늦은 업데이트 함수
+        void LateUpdate() override;
+        // 렌더링 함수
+        void Render(HDC hdc) override;
+
+    private:
+
+    };
+}
