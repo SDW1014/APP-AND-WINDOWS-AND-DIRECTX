a9b3c5bd3bc1ad8e1cefbe78e22557478c92017d
SDW1014 <apif730@gmail.com>
2024-07-18 17:43:45 +0900

    [feat] Change The  myEnums.h

--- WindowApp/Engine_SOURCE/myEnums.h
+++ WindowApp/Engine_SOURCE/myEnums.h
@@ -3,14 +3,28 @@
 
 namespace MyApp::enums
 {
+	/**
+	 * @brief 게임에서 사용되는 레이어 타입을 정의합니다.
+	 */
 	enum class eLayerType
 	{
-		None,
-		BackGround,
-		//Tree 
-		//character
-		Player,
-		Max = 16,
+		None, 		// 아무것도 없는 상태
+		BackGround, // 배경 레이어
+		Tree, 		// 나무 레이어
+		Character, 	// 캐릭터 레이어
+		Player, 	// 플레이어 레이어
+		Max = 16, 	// 최대 레이어 수
+	};
+
+	/**
+	 * @brief 게임에서 사용되는 리소스 타입을 정의합니다.
+	 */
+	enum class eResourceType
+	{
+		Texture,    // 텍스처 리소스
+		AudioClip,  // 오디오 클립 리소스
+		Prefab,     // 프리팹 리소스
+		End,        // 리소스 종료
 	};
 
 }
